fbb6d99e5b3da8cec60ea45a7061e468355762c0
Ali Fadlalla <113270463+alifadl009@users.noreply.github.com>
2023-02-23 13:31:27 +0200

    Update streamlit_yfinance.py

--- streamlit_yfinance.py
+++ streamlit_yfinance.py
@@ -10,5 +10,11 @@ Shown are the stock price and **closing** price and **volume** of Google
 data = yf.Ticker('GOOGL')
 df = data.history(period='1d', start='2010-1-1', end='2023-02-22')
 
+st.write("""
+## Closing Price 
+""")
 st.line_chart(df.Close)
+st.write("""
+## Volume Price
+""")
 st.line_chart(df.Volume)
