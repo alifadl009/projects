c9f88fa28ef50c099857594ddd04fbedf77ca62f
Ali Fadlalla <113270463+alifadl009@users.noreply.github.com>
2023-02-23 04:51:13 +0200

    Add files via upload
    simple stock price of Google

---
+++ streamlit_yfinance.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import pandas as pd
+import streamlit as st
+
+st.write("""
+# Simple stock price app
+Shown are the stock price and closing price and volume of Google
+""")
+
+data = yf.Ticker('GOOGL')
+df = data.history(period='1d', start='2010-1-1', end='2023-02-22')
+
+st.line_chart(df.Close)
+st.line_chart(df.Volume)
